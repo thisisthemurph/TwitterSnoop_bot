aa792717715247c3439c64f8d9acfc15f6261795
Michael Murphy <michael.w.murphy@outlook.com>
2021-10-26 20:51:42 +0100

    Fixed issue with typo in constants

--- telegram_bot/constants.py
+++ telegram_bot/constants.py
@@ -6,7 +6,7 @@ load_dotenv()
 TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
 
 TW_API_KEY: str = os.getenv("TW_API_KEY")
-TW_API_Key_Secret: str = os.getenv("TW_API_Key_Secret")
+TW_API_KEY_SECRET: str = os.getenv("TW_API_KEY_SECRET")
 TW_BEARER_TOKEN: str = os.getenv("TW_BEARER_TOKEN")
 TW_ACCESS_TOKEN: str = os.getenv("TW_ACCESS_TOKEN")
 TW_ACCESS_TOKEN_SECRET: str = os.getenv("TW_ACCESS_TOKEN_SECRET")
