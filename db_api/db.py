731dff6a97362213df0fcb37b4f3b5fc6ecc8e87
Michael Murphy <michael.w.murphy@outlook.com>
2021-10-29 09:36:22 +0100

    Added a Context Manager for the Postgres database connection, updated all functions to utilise this rather than old Db object

--- db_api/db.py
+++ db_api/db.py
@@ -1,50 +1,42 @@
 import psycopg2
 from typing import Union, List
 
-from constants import (DB_HOST, DB_NAME, DB_PASSWORD, DB_USER)
+from constants import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 
 
-class Db:
-    def __init__(self):
+DB_CREDENTIALS = {
+    "host": DB_HOST,
+    "name": DB_NAME,
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+}
+
+
+class Postgres:
+    def __init__(self, host: str, name: str, user: str, password: str):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+
+    def __enter__(self):
         self.conn = psycopg2.connect(
-            host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD
+            host=self.host, database=self.name, user=self.user, password=self.password
         )
         self.cur = self.conn.cursor()
 
-    def execute(self, query: str, vars: tuple) -> None:
-        """
-        Run a query affecting the data in the database
+        return self.conn, self.cur
 
-        Parameters:
-            query (str): the query to be executed
-            vars (tuple): a tuple of the query variables
-        """
-        self.cur.execute(query, vars)
-        self.conn.commit()
-
-    def read(self, query: str, vars: Union[tuple, None] = None):
-        """
-        SELECT data from the database
-
-        Parameters:
-            query (str): the query to be executed
-            vars (tuple): a tuple of the query variables
-        """
-        self.cur.execute(query, vars=vars)
-        return self.cur
-
-    def close(self):
-        """Close the database connection when you're finished!"""
+    def __exit__(self, type, value, traceback):
         self.cur.close()
         self.conn.close()
 
 
 def fetch_all_handles() -> List[str]:
     """Fetches all Twitter handles in the database"""
-    db = Db()
-    cur = db.read("SELECT handle FROM twitter_handles")
-    rows = cur.fetchall()
-    db.close()
+    with Postgres(**DB_CREDENTIALS) as (_, cur):
+        cur.execute("SELECT handle FROM twitter_handles")
+        rows = cur.fetchall()
 
     return [handle[0] for handle in rows]
 
@@ -62,10 +54,9 @@ def fetch_handle_watchers(handle: str) -> List[str]:
     JOIN twitter_handles as h ON whj.handle_id = h._id
     WHERE h.handle = %s;"""
 
-    db = Db()
-    cur = db.read(query, (handle,))
-    rows = cur.fetchall()
-    db.close()
+    with Postgres(**DB_CREDENTIALS) as (_, cur):
+        cur.execute(query, (handle,))
+        rows = cur.fetchall()
 
     return [chat_id[0] for chat_id in rows]
 
@@ -83,11 +74,11 @@ def fetch_watcher_handles(chat_id: str) -> List[str]:
     JOIN twitter_handles as h ON whj.handle_id = h._id
     WHERE w.chat_id = %s"""
 
-    db = Db()
-    cur = db.read(query, (chat_id,))
-    results = cur.fetchall()
+    with Postgres(**DB_CREDENTIALS) as cur:
+        cur.execute(query, (chat_id,))
+        rows = cur.fetchall()
 
-    return [res[0] for res in results]
+    return [handle[0] for handle in rows]
 
 
 def handle_exists(handle: str) -> bool:
@@ -100,10 +91,11 @@ def handle_exists(handle: str) -> bool:
     Returns:
         bool: True if the handle exists, otherwise False
     """
-    db = Db()
-    cur = db.read(f"SELECT handle FROM twitter_handles WHERE handle = %s LIMIT 1", (handle,))
-    result = cur.fetchone()
-    db.close()
+    query = "SELECT handle FROM twitter_handles WHERE handle = %s LIMIT 1"
+
+    with Postgres(**DB_CREDENTIALS) as (_, cur):
+        cur.execute(query, (handle,))
+        result = cur.fetchone()
 
     return result is not None
 
@@ -118,26 +110,25 @@ def watcher_exists(chat_id: str) -> bool:
     Returns:
         bool: True if the chat_id exists, otherwise False
     """
-    db = Db()
-    cur = db.read("SELECT chat_id FROM watchers WHERE chat_id = %s", (chat_id,))
-    result = cur.fetchone()
-    db.close()
+    with Postgres(**DB_CREDENTIALS) as (_, cur):
+        cur.execute("SELECT chat_id FROM watchers WHERE chat_id = %s", (chat_id,))
+        result = cur.fetchone()
 
     return result is not None
 
 
 def add_handle(handle: str) -> None:
     """
-        Add the given handle if it doesn't exist
-    ``
-        Parameters:
-            handle (str): the new handle to be added to the database
+    Add the given handle if it doesn't exist
+
+    Parameters:
+        handle (str): the new handle to be added to the database
 
     """
     if not handle_exists(handle):
-        db = Db()
-        db.execute(f"INSERT INTO twitter_handles (handle) VALUES (%s)", (handle,))
-        db.close()
+        with Postgres(**DB_CREDENTIALS) as (conn, cur):
+            cur.execute("INSERT INTO twitter_handles (handle) VALUES (%s)", (handle,))
+            conn.commit()
 
         return handle_exists(handle)
 
@@ -152,9 +143,9 @@ def add_watcher(chat_id: str) -> None:
         chat_id (str): the ID for the Telegram chat to be added to the database
     """
     if not watcher_exists(chat_id):
-        db = Db()
-        db.execute(f"INSERT INTO watchers (chat_id) VALUES (%s)", (chat_id,))
-        db.close()
+        with Postgres(**DB_CREDENTIALS) as (conn, cur):
+            cur.execute("INSERT INTO watchers (chat_id) VALUES (%s)", (chat_id,))
+            conn.commit()
 
         return watcher_exists(chat_id)
 
@@ -162,27 +153,25 @@ def add_watcher(chat_id: str) -> None:
 
 
 def delete_watched(handle: str, chat_id: str) -> bool:
-    """Delete a relationship between the Twitter handle and Telegram.
+    """Delete a relationship between the Twitter handle and Telegram chat.
 
     Parameters:
         handle (str): the Twitter handle to be watched
         chat_id (str): The chat ID of the Telegram chat doing the watching
 
     """
-    db = Db()
-
     query = "DELETE FROM watcher_handle_join WHERE handle_id = %s AND watcher_id = %s"
+    with Postgres(**DB_CREDENTIALS) as (conn, cur):
+        # Get the ID of the given handle
+        cur.execute("SELECT _id FROM twitter_handles WHERE handle = %s", (handle,))
+        handle_id: int = cur.fetchone()[0]
 
-    # Get the ID of the given handle
-    cur = db.read("SELECT _id FROM twitter_handles WHERE handle = %s", (handle,))
-    handle_id: int = cur.fetchone()[0]
+        # Get the ID of the given watcher
+        cur.execute("SELECT _id FROM watchers WHERE chat_id = %s", (chat_id,))
+        watcher_id: int = cur.fetchone()[0]
 
-    # Get the ID of the given watcher
-    cur = db.read("SELECT _id FROM watchers WHERE chat_id = %s", (chat_id,))
-    watcher_id: int = cur.fetchone()[0]
-
-    db.execute(query, (handle_id, watcher_id))
-    db.close()
+        cur.execute(query, (handle_id, watcher_id))
+        conn.commit()
 
     return True
 
@@ -200,28 +189,27 @@ def watch_handle(handle: str, chat_id: str) -> bool:
     add_handle(handle)
     add_watcher(chat_id)
 
-    db = Db()
+    with Postgres(**DB_CREDENTIALS) as (conn, cur):
+        # Get the ID of the given handle
+        cur.execute("SELECT _id FROM twitter_handles WHERE handle = %s", (handle,))
+        handle_id: int = cur.fetchone()[0]
 
-    # Get the ID of the given handle
-    cur = db.read("SELECT _id FROM twitter_handles WHERE handle = %s", (handle,))
-    handle_id: int = cur.fetchone()[0]
+        # Get the ID of the given watcher
+        cur.execute("SELECT _id FROM watchers WHERE chat_id = %s", (chat_id,))
+        watcher_id: int = cur.fetchone()[0]
 
-    # Get the ID of the given watcher
-    cur = db.read("SELECT _id FROM watchers WHERE chat_id = %s", (chat_id,))
-    watcher_id: int = cur.fetchone()[0]
-
-    # Check if the reationship already exists
-    cur = db.read(
-        "SELECT _id FROM watcher_handle_join WHERE handle_id = %s AND watcher_id = %s LIMIT 1",
-        (handle_id, watcher_id),
-    )
-    result = cur.fetchone()
-
-    if result is None:
-        db.execute(
-            "INSERT INTO watcher_handle_join (handle_id, watcher_id) VALUES (%s, %s);",
+        # Check if the reationship already exists
+        cur.execute(
+            "SELECT _id FROM watcher_handle_join WHERE handle_id = %s AND watcher_id = %s LIMIT 1",
             (handle_id, watcher_id),
         )
 
-    db.close()
+        result = cur.fetchone()
+        if result is None:
+            cur.execute(
+                "INSERT INTO watcher_handle_join (handle_id, watcher_id) VALUES (%s, %s);",
+                (handle_id, watcher_id),
+            )
+            conn.commit()
+
     return True
