25351cd09bb8f16af577a1606e267c254ab4346a
Michael Murphy <michael.w.murphy@outlook.com>
2021-10-29 07:23:11 +0100

    Improved file opening for Linux distros

--- twitter_bot/properties.py
+++ twitter_bot/properties.py
@@ -9,12 +9,12 @@ class Properties:
 
     def _load(self) -> None:
         """Loads the data from the properties file"""
-        with open("properties.json", "r", encoding="utf8") as prop_f:
+        with open("./properties.json", "r", encoding="utf8") as prop_f:
             self._data = json.load(prop_f)
 
     def _save(self) -> None:
         """Saves the new version of the properties file"""
-        with open("properties.json", "w", encoding="utf8") as prop_f:
+        with open("./properties.json", "w", encoding="utf8") as prop_f:
             prop_f.write(json.dumps(self._data, indent=2))
 
         self._load()
